1305ca742ece35edb39459bd81da2105eecd41a7
SISU <atgsisu@gmail.com>
2026-03-05 16:22:36 +0900

    feat: 다이소 상품이 DB에 없을 때 발생하는 오류 원인 로직 수정

--- app.py
+++ app.py
@@ -429,8 +429,8 @@ app.json.ensure_ascii = False
 @app.route('/api/product-detail/<productId>', methods=['GET'])
 def getProductDetail(productId):
     productInfo = db.productInfo.find_one({'productId': productId})
-    productInfo.pop('_id', None)
     if productInfo and datetime.now() <= productInfo.get('ttl', datetime.min):
+        productInfo.pop('_id', None)
         print("cached data is used!!!", productInfo.get("productName"))
         productInfo['ttl'] = productInfo['ttl'].isoformat()
         return jsonify(productInfo)
